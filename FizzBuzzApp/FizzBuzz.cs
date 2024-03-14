c041b9f7616f25da3a09c093c0f42e832e9f2af6
Chris Roberts-Watts <chriscrw@hotmail.co.uk>
2024-03-14 20:14:44 +0000

    Fleshed out the FizzBuzz class to get classes with the IRule interface attached to them and loop through so we can create a base set of rules dynamically.

--- FizzBuzzApp/FizzBuzz.cs
+++ FizzBuzzApp/FizzBuzz.cs
@@ -1,10 +1,43 @@
-﻿namespace FizzBuzzApp
+﻿using RuleInterfaces;
+using System.Reflection;
+
+namespace FizzBuzzApp
 {
     public class FizzBuzz
     {
+        private List<IRule> _rules;
+
+        public FizzBuzz()
+        {
+            _rules = new List<IRule>();
+            this.LoadRules();
+        }
+
+        private void LoadRules()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var types = assembly.GetTypes();
+
+            foreach (var type in types )
+            {
+                if (type.GetInterface("IRule") is not null)
+                {
+                    _rules.Add(Activator.CreateInstance(type) as IRule);
+                }
+            }
+        }
+
         public String GenerateOutput(Int32 number)
         {
-            throw new NotImplementedException();
+            foreach (var rule in _rules)
+            {
+                if (rule.IsMatch(number))
+                {
+                    return rule.GetResult();
+                }
+            }
+
+            return number.ToString();
         }
     }
 }
